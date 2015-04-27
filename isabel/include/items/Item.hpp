0d1bd4a02c4b5f71ee8f4a0b5f549f8f7c9c5282
Leon Cheung <me@leoncheung.com>
2015-04-26 17:39:55 -0700

    move enable item

--- isabel/include/items/Item.hpp
+++ isabel/include/items/Item.hpp
@@ -7,17 +7,20 @@
 //pick up item -> move item from Room to Player
 //drop item -> move item from Player to Room
 //use item -> destroy?
+//solutoin::std::unique_ptr<Item>
 
 class Item
 {
 public:
 	Item();
 	Item(const std::string& name);
+	Item(Item&& other);
 
 	Item(const Item& other) = default;
 	Item& operator=(const Item& other) = default;
 
     const std::string& getName() const;
+    const int getUID() const;
 private:
     const std::string name;
     const int uniqueID;
